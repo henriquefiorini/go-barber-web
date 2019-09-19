8638e32e7d35bb3ac791a5fa29e9bb491b87d9b7
Henrique Fiorini <henriquehfr2@gmail.com>
2019-09-19 09:19:00 -0400

    Redux configuration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
+import Store from './store';
 import History from './services/history';
 import GlobalStyle from './styles';
 import Routes from './routes';
 
 function App() {
   return (
-    <Router history={History}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={Store}>
+      <Router history={History}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
